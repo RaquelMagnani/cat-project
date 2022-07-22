c09dbff09dec73953c50c62548e64a95a5f9c387
Raquel Magnani <quel.magnani88@gmail.com>
2022-07-22 17:21:05 +0200

    changes from interface to type and consumes api from new api file

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,41 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import api from "./services/api";
 
-export interface Breed{
-  name: string
-}
-export interface GetBreedsResponse {
-  data: Breed[]
-}
+export type Breed = {
+  name: string;
+};
+export type GetBreedsResponse = {
+  data: Breed[];
+};
 
-export const getBreeds = async (): Promise<Breed[]|any>  => {
-  try{
-    return await axios.get('https://api.thecatapi.com/v1/breeds');
-  }catch(error){
+export const getBreeds = async (): Promise<Breed[] | any> => {
+  try {
+    console.log("AQUI", api.get("breeds"));
+    return await api.get("breeds");
+  } catch (error) {
     return error;
   }
-}
+};
 
 function App() {
-  
+  const [cats, setCats] = useState<Breed[]>([]);
 
-  
-  const [cats,setCats] = useState<Breed[]>([]);
-
-useEffect(()=>{ 
-  // bora chamar a API
-  getBreeds()
-    .then(({data}) => {
-      setCats(data)
-  })
-
-},[])
+  useEffect(() => {
+    // bora chamar a API
+    getBreeds().then(({ data }) => {
+      setCats(data);
+    });
+  }, []);
   return (
     <div>
-
-     Hello World
-     <ul>
-    {cats.map((item)=>(
-     <li key={`${item.name}`}>{item.name}</li>
-    ))}
-    </ul>
+      Hello World
+      <ul>
+        {cats.map((item) => (
+          <li key={`${item.name}`}>{item.name}</li>
+        ))}
+      </ul>
     </div>
-    
   );
 }
 
