38213a8c9ce4c81f271b38dab64baeefdf38c703
Raquel Magnani <quel.magnani88@gmail.com>
2022-07-27 10:00:48 +0200

    extracts function getBreeds to an isolated file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import api from "./services/api";
+import { useEffect, useState } from 'react';
+import './App.css';
+
+import getBreeds from './services/breeds/getBreeds';
 
 export type Breed = {
   name: string;
@@ -9,20 +10,10 @@ export type GetBreedsResponse = {
   data: Breed[];
 };
 
-export const getBreeds = async (): Promise<Breed[] | any> => {
-  try {
-    console.log("AQUI", api.get("breeds"));
-    return await api.get("breeds");
-  } catch (error) {
-    return error;
-  }
-};
-
 function App() {
   const [cats, setCats] = useState<Breed[]>([]);
 
   useEffect(() => {
-    // bora chamar a API
     getBreeds().then(({ data }) => {
       setCats(data);
     });
