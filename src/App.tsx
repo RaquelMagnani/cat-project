600510700a84579a2585e2de084c0f86bb3b6b5b
Raquel Magnani <quel.magnani88@gmail.com>
2022-08-16 21:14:52 +0200

    creates breedpage and remove logic and style from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,12 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import BreedsList from './components/breedsList';
+import BreedsPage from './components/breedsPage';
 import getBreeds from './services/breeds/getBreeds';
 
-export type Breed = {
-  name: string;
-};
-export type GetBreedsResponse = {
-  data: Breed[];
-};
-
 function App() {
-  const [breeds, setBreed] = useState<Breed[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [isloading, setLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    const loadBreeds = async () => {
-      try {
-        setLoading(true);
-        const response = await getBreeds();
-        setLoading(false);
-        setBreed(response);
-      } catch (error) {
-        setError(error as string);
-      }
-    };
-    loadBreeds();
-  }, [setBreed]);
-  if (error) {
-    return <div>{error}</div>;
-  }
   return (
     <div>
-      <h1>Cats Breeds</h1>
-      {isloading ? <p>loading...</p> : <BreedsList list={breeds} />}
+      <BreedsPage />
     </div>
   );
 }
---
+++ src/components/breedsPage.tsx
@@ -0,0 +1,69 @@
+import { jsx } from '@emotion/react';
+import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
+import getBreeds from '../services/breeds/getBreeds';
+
+export type Props = {
+  list: Breed[];
+};
+export type Breed = {
+  name: string;
+};
+
+const BreedList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const BreedCard = styled.li`
+  border: 1px solid #d9d0dd;
+  border-left: 3px solid #d9d0dd;
+  border-radius: 6px;
+  max-width: 100px;
+  margin: 10px;
+  padding: 10px;
+  list-style: none;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    color: #c39ae4;
+  }
+`;
+
+const BreedsPage = (): JSX.Element => {
+  const [breeds, setBreed] = useState<Breed[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isloading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const loadBreeds = async () => {
+      try {
+        setLoading(true);
+        const response = await getBreeds();
+        setLoading(false);
+        setBreed(response);
+      } catch (error) {
+        setError(error as string);
+      }
+    };
+    loadBreeds();
+  }, [setBreed]);
+  if (error) {
+    return <div>{error}</div>;
+  }
+  if (isloading) return <p>loading...</p>;
+  return (
+    <>
+      <h1>Cats Breeds</h1>
+      <BreedList>
+        {breeds.map((item) => (
+          <BreedCard key={`${item.name}`}>{item.name}</BreedCard>
+        ))}
+      </BreedList>
+    </>
+  );
+};
+
+export default BreedsPage;
