890980965b6aa7192fe9ffed859a95648fb94458
Raquel Magnani <quel.magnani88@gmail.com>
2022-08-29 13:45:28 +0200

    creates and tests getBreedById method

--- src/services/breeds/getBreeds.test.ts
+++ src/services/breeds/getBreeds.test.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError } from 'axios';
-import getBreeds from './getBreeds';
+import { getBreedById, getBreeds } from './getBreeds';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -14,11 +14,16 @@ const mockBreeds = [
     name: 'srd',
   },
 ];
-beforeEach(() => {
-  mockedAxios.get.mockResolvedValue({ data: mockBreeds });
-});
-
+const mockBreed = {
+  name: 'ragdol',
+  id: 'rag',
+  origin: 'US',
+  description: 'Ragdolls love their people, greeting them at the door',
+};
 describe('getBreeds', () => {
+  beforeEach(() => {
+    mockedAxios.get.mockResolvedValue({ data: mockBreeds });
+  });
   it('Should return a list with 3 cat breeds when API/breeds call is successful', async () => {
     const result = await getBreeds();
     expect(mockedAxios.get).toHaveBeenCalledWith(
@@ -41,6 +46,28 @@ describe('getBreeds', () => {
     await expect(getBreeds()).rejects.toThrow();
     expect(mockedAxios.get).toHaveBeenCalledWith(
       'https://api.thecatapi.com/v1/breeds'
+    );
+  });
+});
+
+describe('getBreedById', () => {
+  beforeEach(() => {
+    mockedAxios.get.mockResolvedValue({ data: mockBreed });
+  });
+  it('Should return cat infos when API/breedsById call is successful', async () => {
+    const result = await getBreedById('rgd');
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      'https://api.thecatapi.com/v1/breeds/rgd'
+    );
+    expect(result).toEqual(mockBreed);
+  });
+
+  it('should return an error message when API fails ', async () => {
+    const message = 'Aconteceu um erro inesperado';
+    mockedAxios.get.mockRejectedValueOnce(new Error(message));
+    await expect(getBreedById('rgd')).rejects.toThrow(message);
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      'https://api.thecatapi.com/v1/breeds/rgd'
     );
   });
 });
--- src/services/breeds/getBreeds.tsx
+++ src/services/breeds/getBreeds.tsx
@@ -1,12 +1,16 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 export type Breed = {
   name: string;
+  id: string;
+  origin: string;
+  description: string;
 };
 export type GetBreedsResponse = {
   data: Breed[];
 };
 
-const getBreeds = async (): Promise<Breed[]> => {
+export const getBreeds = async (): Promise<Breed[]> => {
   try {
     const { data } = await axios.get('https://api.thecatapi.com/v1/breeds');
     return data;
@@ -19,4 +23,17 @@ const getBreeds = async (): Promise<Breed[]> => {
   }
 };
 
-export default getBreeds;
+export const getBreedById = async (id: string): Promise<Breed> => {
+  try {
+    const { data } = await axios.get(
+      `https://api.thecatapi.com/v1/breeds/${id}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.message);
+    } else {
+      throw new Error('Aconteceu um erro inesperado');
+    }
+  }
+};
